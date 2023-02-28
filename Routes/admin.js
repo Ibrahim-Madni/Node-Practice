eca49cf6273bec6d24c1685f1be02b19bc94ea26
Ibrahim-Madni <ibrahim.madni@hotmail.com>
2023-02-28 11:37:04 +0500

    new changes

--- Routes/admin.js
+++ Routes/admin.js
@@ -1,15 +1,17 @@
-import express from 'express';
+import express from "express";
 
-const adminRouter =  express.Router()
+const adminRouter = express.Router();
 
-adminRouter.use('/addproduct',(req, res, next)=>{
-    console.log('In another middleware')
-    res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></input></form>')
+adminRouter.use("/addproduct", (req, res, next) => {
+  console.log("In another middleware");
+  res.send(
+    '<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></input></form>'
+  );
 });
 
-adminRouter.post('/product', (req, res, next)=>{
-    console.log(req.body);
-    res.redirect('/');
-})
+adminRouter.post("/product", (req, res, next) => {
+  console.log(req.body);
+  res.redirect("/");
+});
 
-export default adminRouter
+export default adminRouter;
