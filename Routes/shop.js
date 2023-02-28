ac15a7c4f10f0fedf2cefa9cb02f3c633fdb6da0
Ibrahim-Madni <ibrahim.madni@hotmail.com>
2023-02-28 12:13:54 +0500

    console log update

--- Routes/shop.js
+++ Routes/shop.js
@@ -7,5 +7,6 @@ const shopRouter = express.Router();
 shopRouter.get("/", (req, res, next) => {
   res.sendFile(path.join(__dirname, "../", "views", "shop.html"));
 });
+console.log("here")
 
 export default shopRouter;
